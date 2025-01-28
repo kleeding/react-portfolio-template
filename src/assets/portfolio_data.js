1bf802c7df240cac4dad9ed360ad83ef833dcaf7
kleeding <kawleeding@gmail.com>
2025-01-28 15:33:15 +0000

    updated data

---
+++ src/assets/portfolio_data.js
@@ -0,0 +1,30 @@
+const webdata = {
+    header: {
+        "name": "John Doe",
+        "description": "Software Developer",
+        "socials": [
+            {   name:"github", 
+                url:"https://github.com/",
+                alt: "Github Logo"
+            },
+            {   name:"linkedin", 
+                url:"https://linkedin.com/",
+                alt: "Linkedin Logo"
+            },
+            {   name:"email", 
+                url:"https://gmail.com/",
+                alt: "Email Logo"
+            },
+            {   name:"twitter", 
+                url:"https://twitter.com/",
+                alt: "Github Logo"
+            },
+            {   name:"leetcode", 
+                url:"https://leetcode.com/",
+                alt: "Leetcode Logo"
+            }
+        ]
+    }
+}
+
+export default webdata
--- src/assets/webdata.js
+++
@@ -1,8 +0,0 @@
-const webdata = {
-    header: {
-        "name": "John Doe",
-        "description": "Software Developer"
-    }
-}
-
-export default webdata
