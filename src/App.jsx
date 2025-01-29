b3d136ed7230e66487ca92bcbf9c36cb259a87f1
kleeding <kawleeding@gmail.com>
2025-01-29 15:15:44 +0000

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
 import Header from './components/Header/Header'
 import './App.css'
 import data from './assets/portfolio_data'
@@ -6,6 +7,8 @@ import data from './assets/portfolio_data'
 function App() {
   return (
     <>
+      <section className="page-top">
+        <Navbar {...data.navbar} />
       <Header {...data.header} />
       {/* <Header name="First React Page"/>
       <Header name="cool stuff"/> */}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import {useState} from 'react';
+import './Navbar.css'
+
+function NavElement(props) {
+    return (
+        <a className="page-link" href={props.entry.url}>{props.entry.name}</a>
+    )
+}
+
+function Navbar(props) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen((open) => !open)
+    }
+
+    const navbarElements = props.links.map((entry) => {
+        return (
+            <NavElement key={entry.id} entry={entry} />
+        )
+    })
+
+    return (
+        <nav className='nav-elements'>
+            <li className="nav-logo">
+                <div className='tmp-logo'>
+                    <a  href='#header'>LOGO</a>
+                </div>
+            </li>
+
+            <div className='nav-links'>
+                <div className={`nav-mobile-trigger-open  ${isOpen ? "is-open" : ""}`} onClick={toggleMenu}>
+                    <div className='nav-burger'></div>
+                    <div className='nav-burger'></div>
+                    <div className='nav-burger'></div>
+                </div>
+                
+                <div className={`nav-mobile-trigger-close  ${isOpen ? "is-open" : ""}`} onClick={toggleMenu}>
+                    <div className='nav-cross-1'></div>
+                    <div className='nav-cross-2'></div>
+                </div>
+
+                <ul className={`links-container ${isOpen ? "is-open" : ""}`}>
+                    {navbarElements}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
