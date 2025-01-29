68f62311e0ef477a1c89702f42b84523b0d2a273
kleeding <kawleeding@gmail.com>
2025-01-29 15:16:02 +0000

    navbar info

--- src/assets/portfolio_data.js
+++ src/assets/portfolio_data.js
@@ -1,29 +1,71 @@
 const webdata = {
+    navbar: {
+        links: [
+            {
+                id: 1,
+                name: "ABOUT",
+                url: "#about"
+            },
+            {
+                id: 2,
+                name: "SKILLS",
+                url: ""
+            },
+            {
+                id: 3,
+                name: "PROJECTS",
+                url: ""
+            },
+            {
+                id: 4,
+                name: "EXPERIENCE",
+                url: "#experience"
+            },
+            {
+                id: 5,
+                name: "CONTACT",
+                url: ""
+            }
+        ]
+    },
     header: {
-        "name": "John Doe",
-        "description": "Software Developer",
-        "socials": [
-            {   name:"github", 
+        name: "John Doe",
+        description: "Software Developer",
+        socials: [
+            {   
+                id: 1,
+                name:"github", 
                 url:"https://github.com/",
                 alt: "Github Logo"
             },
-            {   name:"linkedin", 
+            {   
+                id: 2,
+                name:"linkedin", 
                 url:"https://linkedin.com/",
                 alt: "Linkedin Logo"
             },
-            {   name:"email", 
+            {   
+                id: 3,
+                name:"email", 
                 url:"https://gmail.com/",
                 alt: "Email Logo"
             },
-            {   name:"twitter", 
+            {   
+                id: 4,
+                name:"twitter", 
                 url:"https://twitter.com/",
                 alt: "Github Logo"
             },
-            {   name:"leetcode", 
+            {   
+                id: 5,
+                name:"leetcode", 
                 url:"https://leetcode.com/",
                 alt: "Leetcode Logo"
             }
         ]
+    },
+    about: {
+        title: "ABOUT ME"
     }
 }
 
